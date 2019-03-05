a1776d61140b2503470380a1f1b75585b4b7049b
shike.miao@gmail.com <shike.miao@gmail.com>
2019-03-05 00:04:12 -0600

    add pricelist
    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
+import PriceList from './components/priceList';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
+const items = [{
+  "id": 1,
+  "title": "Travel to abroad",
+  "price": 100,
+  "date": "2019-03-03",
+  "category": {
+    "id": 1,
+    "name": "trip",
+    "type": "outcome"
+      }
+},{
+  "id": 2,
+  "title": "Travel to abroad",
+  "price": 200,
+  "date": "2019-03-03",
+  "category": {
+    "id": 2,
+    "name": "food",
+    "type": "outcome"
+  }
+  }
+]
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <PriceList
+            items={items}
+            onModifyItem={item=>console.log(item.id)}
+            onDeleteItem={item=>console.log(item.id)}
+          />
       </div>
     );
   }
