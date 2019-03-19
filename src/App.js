da04ac3e0e44b7ea3ce922a1fe035d78468a0c43
shike.miao@gmail.com <shike.miao@gmail.com>
2019-03-18 23:52:35 -0500

    built router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import Home from './pages/home';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Example from './components/example';
 import './App.css';
 
 class App extends Component {
   
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <Router>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/exam'>Example</Link>
+        </div>
+        <Route path='/' exact component={Home} />
+        <Route path='/exam' exact component={Example} />
+        <Route path='/exam/:name' exact component={Example} />
+      </Router>
+      
     );
   }
 }
